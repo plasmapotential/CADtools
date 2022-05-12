10e471fd58db1be327f10d11ec5dccbad7fde72d
plasmapotential <thomaslooby@gmail.com>
2022-05-12 12:46:54 -0400

    ipv4 NAT for windows

--- crossSections2Mesh.py
+++ crossSections2Mesh.py
@@ -13,6 +13,7 @@ from shapely.geometry import MultiPoint, MultiPolygon, Polygon, LinearRing
 import plotly.graph_objects as go
 import os
 import sys
+import argparse
 
 #============== user inputs ===================
 rMax = 5000
@@ -21,7 +22,7 @@ phi = 90.0 #degrees
 grid_size = 200
 
 #path where CAD .step file lives
-rootPath = '/home/tom/work/CFS/projects/reducedCAD/vacVes/'
+rootPath = '/root/source/CADtools/'
 STPfile = rootPath + 'vacVes.step'
 
 #outputs
@@ -31,12 +32,12 @@ STPout2D = rootPath + 'vacVesout2D.step'
 pTableOut = rootPath + 'pTable.csv'
 
 #HEAT path
-HEATpath = '/home/tom/source/HEAT/github/source'
+HEATpath = '/root/source/HEAT'
 #=============================================
 
 #load HEAT into the path envVar
 sys.path.append(HEATpath)
-
+print(sys.path)
 #load HEAT environment
 import launchHEAT
 launchHEAT.loadEnviron()
@@ -330,4 +331,24 @@ if dashApp == True:
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    #parse command line arguments
+    parser = argparse.ArgumentParser(description=""" Use this command to launch crossSections2Mesh """)
+    parser.add_argument('--a', type=str, help='IP address ', required=False)
+    parser.add_argument('--p', type=str, help='port # ', required=False)
+    args = parser.parse_args()
+    address = vars(args)['a']
+    port = vars(args)['p']
+    #use default IPv4 address and port unless user provided one
+    if address == None:
+        address = '127.0.0.1' #default
+    if port == None:
+        port = 8050 #default
+
+    app.run_server(
+                    debug=True,
+                    dev_tools_ui=True,
+                    port=port,
+                    host=address,
+                    use_reloader=False, #this can be used in local developer mode only
+                    dev_tools_hot_reload = False,
+                    )
